d4811c9b7b5840fcf6c656bf9f651b993f05fa6e
ngoa <ngoa@csc4102022.cs.berea.edu>
2022-11-09 11:39:10 -0500

    fix bugs and redesign with abstraction

--- genome.cpp
+++ genome.cpp
@@ -4,104 +4,88 @@
 
 using namespace std;
 
-struct Pixel {
-  int red; 
-  int blue;
-  int green;
+genome::genome() {
+  // Constructor
+  genes = NULL;
+  nGenes = 0;
 };
 
-class genome {
+genome::~genome() {
+  // Destructor
+  delocate();
+}; 
 
-  public:
-    genome() {
-      // Constructor
-      genes = NULL;
-      nGenes = 0;
-    }; 
-
-    ~genome() {
-      // Destructor
-      delocate()
-    }; 
-    
-    void allocate(int nGenes) {
-      // Take input as how many Pixels to store, 
-      // allocates space for them, and initializes all the RGB Pixel values to zero
-      if (this->genes != NULL) {
-        delocate();
-      }
-      
-      genes = new Pixel[nGenes];
-      this->nGenes = nGenes;
-    }; 
-
-    void deallocate() {
-      // free up the space that is pointed to by genes and sets nGenes to zero
-      if (this->genes == NULL) {
-        return;
-      }
-      this->genes = new Pixel;
-      delete this->genes;
-      this->genes = NULL;
-      
-      this->nGenes = new int;
-      delete this->nGenes;
-      this->nGenes = 0;
-      
-    }; 
-    
-    void randomize() {
-      // set random values between 0 and 256 for each of the Red, Blue, and Green 
-      // variables for each Pixel in the genes array
-      for (int i = 0; i < this->nGenes; i++) {
-        setRed(i, rand() % 256 + 1);
-        setGreen(i, rand() % 256 + 1);
-        setBlue(i, rand() % 256 + 1);
-      };
-    }; 
-    
-    void setRed(int index, int value) {
-      // set Red color
-      this->genes[index].red = value;
-    }; 
-    
-    void setGreen(int index, int value) {
-      // set Green color
-      this->genes[index].green = value;
-    }; 
-    
-    void setBlue(int index, int value) {
-      // set Blue color
-      this->genes[index].blue = value;
-    }; 
-    
-    int getRed(int index) {
-      // get Red
-      return this->genes[index].red;
-    }; 
-    
-    int getGreen(int index) {
-      // get Green
-      return this->genes[index].green;
-    }; 
-    
-    int getBlue(int index) {
-      // get Blue
-      return this->genes[index].blue;
-    }; 
-    
-    void print() {
-      cout << this->nGenes << ' genes';
-      for (int i = 0, i < nGenes, i ++) {
-        cout << "(" << this->genes[i].red << "," << this->genes[i].blue  << "," << this->genes[i].green << ")" << endl;
-      };
-    }; 
-
+void genome::allocate(int nGenes) {
+  // Take input as how many Pixels to store, 
+  // allocates space for them, and initializes all the RGB Pixel values to zero
+  if (this->genes != NULL) {
+    delocate();
+  }
   
-  private:
-    Pixel* genes;
-    int nGenes;
-    
-};
+  genes = new Pixel[nGenes];
+  this->nGenes = nGenes;
+}; 
+
+void genome::delocate() {
+  // free up the space that is pointed to by genes and sets nGenes to zero
+  if (this->genes == NULL) {
+    return;
+  }
+  
+  delete[] this->genes;
+  this->genes = NULL;
+  this->nGenes = 0;
+  
+}; 
+
+
+void genome::randomize() {
+  // set random values between 0 and 256 for each of the Red, Blue, and Green 
+  // variables for each Pixel in the genes array
+  for (int i = 0; i < this->nGenes; i++) {
+    setRed(i, rand() % 256 + 1);
+    setGreen(i, rand() % 256 + 1);
+    setBlue(i, rand() % 256 + 1);
+  };
+}; 
+
+void genome::setRed(int index, int value) {
+  // set Red color
+  this->genes[index].red = value;
+}; 
+
+void genome::setGreen(int index, int value) {
+  // set Red color
+  this->genes[index].green = value;
+}; 
+
+void genome::setBlue(int index, int value) {
+  // set Red color
+  this->genes[index].blue = value;
+}; 
+
+int genome::getRed(int index) {
+  // get Red
+  return this->genes[index].red;
+}; 
+
+int genome::getGreen(int index) {
+  // get Red
+  return this->genes[index].green;
+}; 
+
+int genome::getBlue(int index) {
+  // get Red
+  return this->genes[index].blue;
+}; 
+
+void genome::print() {
+  cout << "GENOME: " << this->nGenes << " genes" << endl;
+   for (int i = 0; i < nGenes; i ++) {
+    cout << "(" << this->genes[i].red << "," << this->genes[i].blue  << "," << this->genes[i].green << ")" << endl;
+  };
+  cout;
+}; 
+
 
 
--- genome.h
+++ genome.h
@@ -2,7 +2,7 @@ struct Pixel {
   int red; 
   int blue;
   int green;
-}
+};
 
 class genome {
 
@@ -10,18 +10,17 @@ class genome {
     genome(); // Constructor
     ~genome(); // Destructor
     void allocate(int nGenes); // Take input as how many Pixels to store, allocates space for them, and initializes all the RGB Pixel values to zero
-    void deallocate(); // free up the space that is pointed to by genes and sets nGenes to zero
+    void delocate(); // free up the space that is pointed to by genes and sets nGenes to zero
     void randomize(); // set random values between 0 and 256 for each of the Red, Blue, and Green variables for each Pixel in the genes array
     void setRed(int index, int value); // set Red color
     void setGreen(int index, int value); // set Green color
     void setBlue(int index, int value); // set Blue color
-    int getRed(int index, int value); // get Red
-    int getGreen(int index, int value); // get Green
-    int getBlue(int index, int value); // get Blue
+    int getRed(int index); // get Red
+    int getGreen(int index); // get Green
+    int getBlue(int index); // get Blue
     void print(); // print out the genes
   
   private:
     Pixel* genes;
     int nGenes;
-  
 };
